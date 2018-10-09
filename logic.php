6b8becb6206b1379ab494108ec103bcb73b7054f
Rupert <rupert.dudeney@gmail.com>
2018-10-09 07:31:17 -0400

    Simplified code for logic.php

--- logic.php
+++ logic.php
@@ -1,20 +1,21 @@
 <?php
+
+/*
+ * This is the logic file for index.php; it's job is to check the
+ * SESSION for results, and if available, store the results in variables
+ * to be displayed in index.php
+ */
+
+
 session_start();
 
 $hasErrors = false;
 
+# Get `results` data from session, if available
 if (isset($_SESSION['results'])) {
     $results = $_SESSION['results'];
 
-    $type = $results['type'];
-    $repetitions = $results['repetitions'];
-    $guess = $results['guess'];
-    $percentage = $results['percentage'];
-    $num_correct = $results['num_correct'];
-    $response = $results['response'];
-
-    $errors = $results['errors'];
-    $hasErrors = $results['hasErrors'];
+    extract($results);
 }
 
 session_unset();
